da8f63ec88e9e4c331a5ff357c15b262fb1b3fab
Alan <alan@stolc.net>
2019-01-22 22:09:42 +0100

    Saving work, DB connection is working

--- app/src/main/java/com/example/vraky/MapsActivity.java
+++ app/src/main/java/com/example/vraky/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.vraky;
 
+import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -19,6 +20,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Marker;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,6 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Curl in main thread needs this
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     @Override
@@ -46,6 +59,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+
+                System.out.println("Map long clicked");
 
                 final Marker markerName = mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory
                         .defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
@@ -67,8 +82,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         String brand_selected = brandsSpinner.getSelectedItem().toString();
                         String colour_selected = coloursSpinner.getSelectedItem().toString();
                         String condition_selected = conditionSpinner.getSelectedItem().toString();
+                        if (condition_selected.equals("Nepojízdné")) {
+                            condition_selected = "0";
+                        } else {
+                            condition_selected = "1";
+                        }
+                        ;
 
                         markerName.setTitle(colour_selected.concat(" auto značky ").concat(brand_selected));
+
+                        // create String with urlParameters
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("brand=").append(brand_selected).append("&colour=").append(colour_selected);
+                        sb.append("&status=").append(condition_selected);
+                        System.out.println(condition_selected);
+                        String urlParameters = sb.toString();
+
+                        try {
+                            URL url = new URL("http://www.stolc.net/insert.php");
+                            System.out.println(getResponseFromHttpUrl(url, urlParameters));
+                        } catch (Exception e) {
+                            System.out.println(e.fillInStackTrace());
+                        }
                     }
                 });
 
@@ -83,4 +118,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
+
+    public static String getResponseFromHttpUrl(URL url, String urlParameters) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("charset", "utf-8");
+        connection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        connection.setUseCaches(false);
+        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+            wr.write(postData);
+        }
+
+        try {
+            InputStream in = connection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            connection.disconnect();
+        }
+    }
+
 }
